2b9143a707ea3071257556f9d6e709c0b21a47bf
Smnthjm08 <sumanthjmm.04@gmail.com>
2025-12-19 20:10:18 +0530

    users endpoint get and post req added

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,71 @@
-import express from "express";
+import express, { type Request, type Response } from "express";
+import { prisma } from "./lib/prisma";
 
 const app = express();
 
-app.get("/cpu", (req, res) => {
-	for (let i = 0; i < 100000000; i++) {
-		Math.random();
-	}
-	res.send("Hello world");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const PORT = process.env.PORT || 8081;
+
+app.get("/", (req, res) => {
+  res.json({ message: "Hello, World!" });
 });
 
-app.get("/", (req, res)=>{
-    // health check
-    res.send("hii");
-})
+app.get("/health", (req: Request, res: Response) => {
+  res.send({
+    status: "OK",
+    timestamp: new Date().toISOString(),
+    env: process.env.ENV!,
+  });
+});
 
-app.listen(3000)
+app.get("/users", async (req: Request, res: Response) => {
+  try {
+    const users = await prisma.user.findMany();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch users" });
+    return;
+  }
+});
+
+app.post("/users", async (req: Request, res: Response) => {
+  try {
+    const { username, name, email, password } = req.body;
+
+    if (!username || !name || !email || !password) {
+      res.status(400).json({ error: "Missing required fields" });
+      return;
+    }
+
+    const existingUser = await prisma.user.findUnique({
+      where: { username, email },
+    });
+
+    if (existingUser) {
+      res
+        .status(409)
+        .json({ error: "User with username or email already exists" });
+      return;
+    }
+
+    const newUser = await prisma.user.create({
+      data: {
+        username: username,
+        name: name,
+        email: email,
+        password: password,
+      },
+    });
+    res.status(201).json(newUser);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Failed to create user" });
+    return;
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
