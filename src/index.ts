b91e0690b87455747bdf16631974a89038457fe3
Smnthjm08 <sumanthjmm.04@gmail.com>
2025-12-21 17:39:56 +0530

    removed ci.yml and added docker push after the build at github actions

--- src/index.ts
+++ src/index.ts
@@ -66,6 +66,25 @@ app.post("/users", async (req: Request, res: Response) => {
   }
 });
 
+app.get("/user/:id", async (req: Request, res: Response) => {
+  const { id } = req.params;
+  try {
+    const user = await prisma.user.findUnique({
+      where: { id: String(id) },
+    });
+
+    if (!user) {
+      res.status(404).json({ error: "User not found" });
+      return;
+    }
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch user" });
+    return;
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
