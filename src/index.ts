c554a256d5eb485d35a6aa4ba0119b314ad6e9d4
Smnthjm08 <sumanthjmm.04@gmail.com>
2026-02-19 23:53:23 +0530

    nodeport service and deployment yml for k8s added

--- src/index.ts
+++ src/index.ts
@@ -24,71 +24,7 @@ app.get("/health", (req: Request, res: Response) => {
   });
 });
 
-app.get("/users", async (req: Request, res: Response) => {
-  try {
-    const users = await prisma.user.findMany();
-    res.status(200).json(users);
-  } catch (error) {
-    res.status(500).json({ error: "Failed to fetch users" });
-    return;
-  }
-});
-
-app.post("/users", async (req: Request, res: Response) => {
-  try {
-    const { username, name, email, password } = req.body;
-
-    if (!username || !name || !email || !password) {
-      res.status(400).json({ error: "Missing required fields" });
-      return;
-    }
-
-    const existingUser = await prisma.user.findUnique({
-      where: { username, email },
-    });
-
-    if (existingUser) {
-      res
-        .status(409)
-        .json({ error: "User with username or email already exists" });
-      return;
-    }
-
-    const newUser = await prisma.user.create({
-      data: {
-        username: username,
-        name: name,
-        email: email,
-        password: password,
-      },
-    });
-    res.status(201).json(newUser);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Failed to create user" });
-    return;
-  }
-});
-
-app.get("/user/:id", async (req: Request, res: Response) => {
-  const { id } = req.params;
-  try {
-    const user = await prisma.user.findUnique({
-      where: { id: String(id) },
-    });
-
-    if (!user) {
-      res.status(404).json({ error: "User not found" });
-      return;
-    }
-
-    res.status(200).json(user);
-  } catch (error) {
-    res.status(500).json({ error: "Failed to fetch user" });
-    return;
-  }
-});
-
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,5 +1,14 @@
 import { Router } from "express";
+import {
+    createUser,
+    getUserById,
+    getUsers,
+} from "../controllers/user.controller";
 
 const userRoutes = Router();
+
+userRoutes.get("/", getUsers);
+userRoutes.post("/", createUser);
+userRoutes.get("/:id", getUserById);
 
 export default userRoutes;
